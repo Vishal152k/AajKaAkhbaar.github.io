e01695f584ff9439e5b215adea669e6d7cc878d8
Vishesh2k01 <62540995+Vishesh2k01@users.noreply.github.com>
2020-06-23 17:37:15 +0530

    Update custom2js.js

--- bs/custom2js.js
+++ bs/custom2js.js
@@ -11,34 +11,50 @@
 		$scope.uploadExcel= function(){
 
 			var myFile = document.getElementById('file');
+			console.log(myFile)
 			var input = myFile;
 			var reader = new FileReader();
+			console.log(reader)
 			reader.onload = function(){
 				var fileData = reader.result;
 				var workbook = XLSX.read(fileData, {type:'binary'});
 				workbook.SheetNames.forEach(function(sheetName){
 					var rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
 					excelJsonObj = rowObject;
+					$("#upload_button").remove();
+					$("#file").remove();
 				});
-
+				$('#myTable:last-child').append("");
 				for( var i=0; i< excelJsonObj.length; i++){
 					
 					var data = excelJsonObj[i];
-					var str = data.TITLE;
-					var img_URL = data.IMAGE;
+					var str = data.title;
+					
+					var img_URL = data.url_image;
 					var str2 = 'image';
 					
 
 
-					link = data.URL;
+					link = data.url;
+					var read_more = "read more";
 
-					$('#myTable tbody:last-child').append("<tr><td>"+str.link(link)+"</td><td>"+'<img src="' + img_URL + '" height="250px" width="250px">'+"</td></tr>");
+					$('#myTable:last-child').append(
+						'<div class="card">'+
+ 						'<a href="'+data.url+'">'+'<img src="'+img_URL+'" alt="Avatar" style="width:100%;">'+'</a>'
+  						
+    					
+    					+'<a href="'+data.url+'"><p style="font-size:30px;font-style:italic;">'+data.title+'</p></a>'+
+    					data.description+read_more.link(link)
+
+  						
+						+"</div>"+"<br>");
 
 				}
 
 			};
-
+			input.files[0].name="Positive_News.xlsx"
 			reader.readAsBinaryString(input.files[0]);
+			console.log(input.files[0])
 
 		};
 	}
